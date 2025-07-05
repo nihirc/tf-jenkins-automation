00d03d5c67f19554bc12fae09c161529a580badd
Nihir Chadderwala <nihirc@live.com>
2025-07-05 12:13:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "chadderw-tf-jenkins-automation"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-  }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "chadderw-tf-jenkins-automation"
 }
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
