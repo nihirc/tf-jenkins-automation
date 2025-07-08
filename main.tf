d8a212ce9d8095ced75d03e9ea3e4af93ed7c486
Nihir Chadderwala <nihirc@live.com>
2025-07-08 09:43:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,64 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "chadderw-tf-jenkins-automation"
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "chadderw-s3-temp"
+  
+  # Bucket versioning is required for GxP, SOX, HIPAA compliant accounts
+  versioning {
+    enabled = true
   }
+
+  # Tags for resource identification
+  tags = {
+    Name        = "SecureBucket"
+    Environment = "Production"
+  }
+}
+
+# Block public access (required by security guidelines)
+resource "aws_s3_bucket_public_access_block" "secure_bucket_public_access_block" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Enable default encryption at rest with AES256
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_bucket_encryption" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Enforce encryption in transit
+resource "aws_s3_bucket_policy" "secure_bucket_policy" {
+  bucket = aws_s3_bucket.secure_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnforceEncryptionInTransit"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.secure_bucket.arn,
+          "${aws_s3_bucket.secure_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
 }
